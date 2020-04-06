bb1d38515f2083d0725b8885fb058d38663f3148
diegodsgarcia <diego.ds.garcia@gmail.com>
2020-04-06 18:18:21 -0300

    chore: add analytics

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,4 +17,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
+
+ReactGA.initialize('UA-162943959-1')
+ReactGA.pageview(window.location.pathname + window.location.search)
