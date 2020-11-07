d0944bbb33b5c8b4a7ab000fe47501598f55801d
diegodsgarcia <diego.ds.garcia@gmail.com>
2020-11-07 15:39:24 -0300

    chore: configure public folder and metatags

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -7,13 +7,31 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <title>Anime Season</title>
+        <meta name="theme-color" content="#DDDDDD" />
+        <meta name="description" content="Anime Season website" />
+
+        <meta property="og:url" content="https://anime-season.now.sh/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Anime Season" />
         <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          property="og:description"
+          content="A simple project web to can see the Animes of Season"
+        />
+        <meta
+          property="og:image"
+          content="https://anime-season.now.sh/logo512.png"
+        />
+        <meta property="fb:app_id" content="2686422261647500" />
+
+        <meta name="twitter:site" content="@diegodsgarcia" />
+        <meta
+          name="twitter:description"
+          content="A simple project web to can see the Animes of Season"
+        />
+        <meta
+          name="twitter:image"
+          content="https://anime-season.now.sh/logo512.png"
         />
       </Head>
       <GlobalStyles />
